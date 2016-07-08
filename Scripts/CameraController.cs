5ffb17c0271b5f5df19f73feab14aad576eaf1ab
josephquested <josephquested@gmail.com>
2016-07-08 18:13:53 +1200

    initial commit

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	Transform target;
+	public float distance;
+
+	void Start ()
+	{
+		target = GameObject.FindWithTag("Player").transform;
+	}
+
+	void FixedUpdate ()
+	{
+		transform.position = new Vector3(
+			target.position.x,
+			20,
+			target.position.z - distance
+		);
+	}
+}
